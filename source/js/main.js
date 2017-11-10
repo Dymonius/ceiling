3e29375bb33323260ef99ce9155c0c869674ebeb
Dmitry <dymonius12@gmail.com>
2017-11-10 13:22:47 +0200

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,126 @@
+window.onload = function () {
+
+    //Calculator
+    (function () {
+        var square = document.querySelector('.square');
+        var angle = document.querySelector('.quantity');
+        var radio1 = document.querySelector('.radio1');
+        var radio2 = document.querySelector('.radio2');
+        var radio3 = document.querySelector('.radio3');
+        var price = document.querySelector('.form__price');
+
+
+        var anglePrice = 70;
+        var materialPrice = 100;
+
+        price.innerHTML = square.value * materialPrice + angle.value * anglePrice + ' грн.';
+
+
+        function result() {
+
+            price.innerHTML = square.value * materialPrice + angle.value * anglePrice + ' грн.';
+        }
+
+        square.addEventListener('input', result);
+        angle.addEventListener('input', result);
+        radio1.addEventListener('click', function () {
+            materialPrice = 100;
+            result();
+        });
+        radio2.addEventListener('click', function () {
+            materialPrice = 150;
+            result();
+        });
+        radio3.addEventListener('click', function () {
+            materialPrice = 200;
+            result();
+        })
+    })();
+
+    //Sticky Menu
+    (function () {
+        var stickElement = document.querySelector(".menu");
+
+        var fixed = false;
+        var fixPoint = stickElement.offsetTop + stickElement.offsetHeight; //Получение расстояния по оси Y от верха страницы до элемента
+
+        window.onscroll = function () {
+            if (window.innerWidth > 1000) {
+                var distance = fixPoint - window.pageYOffset;
+                var offset = window.pageYOffset; //Получение текущей прокрутки сверху
+                if ((distance <= 0) && !fixed) {
+                    stickElement.classList.add('menu--fixed');
+
+                    fixed = true;
+                } else if (fixed && (offset <= fixPoint)) {
+                    stickElement.classList.remove('menu--fixed');
+
+                    fixed = false;
+                }
+            }
+        };
+    })();
+
+    // Up-Down button
+    (function () {
+        'use strict';
+
+        var upDownBtn = document.querySelector('.up_down_btn');
+        var check;
+
+        function trackScroll() {
+            var scrolled = window.pageYOffset;
+            var coords = document.documentElement.clientHeight;
+
+            if (scrolled > coords) {
+                upDownBtn.classList.add('up_down_btn-show');
+                upDownBtn.setAttribute('title', 'Наверх');
+                check = false;
+            }
+            else if (scrolled <= coords) {
+                upDownBtn.classList.remove('up_down_btn-show');
+                upDownBtn.setAttribute('title', 'Наверх');
+                check = false;
+            }
+        }
+
+        function backToTop() {
+            upDownBtn.classList.add('up_down_btn-disabled');
+            if (!check) {
+                (function goTop() {
+
+                    if (window.pageYOffset !== 0) {
+                        window.scrollBy(0, -40);
+                        setTimeout(goTop, 0);
+                    } else {
+                        upDownBtn.classList.remove('up_down_btn-disabled');
+                    }
+
+                })();
+                return;
+
+            } else if (check) {
+                (function goBottom() {
+                    var match = Math.ceil(window.pageYOffset + document.documentElement.clientHeight);
+
+                    if (match != document.documentElement.scrollHeight) {
+                        window.scrollBy(0, 80);
+                        setTimeout(goBottom, 0);
+                    } else {
+                        upDownBtn.classList.remove('up_down_btn-disabled');
+                    }
+
+                })();
+                return;
+            }
+
+        }
+
+        window.addEventListener('scroll', trackScroll);
+        upDownBtn.addEventListener('click', backToTop);
+    })();
+};
+
+
+
+
